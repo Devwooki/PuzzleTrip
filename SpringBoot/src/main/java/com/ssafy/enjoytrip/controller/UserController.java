6296f100098be455f875262d48cb173c5ac80209
gmzuddl <>
2023-05-10 01:42:58 +0900

    [Update] : 로그인 기능 Vue로 ajax구현하기

--- SpringBoot/src/main/java/com/ssafy/enjoytrip/controller/UserController.java
+++ SpringBoot/src/main/java/com/ssafy/enjoytrip/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ssafy.enjoytrip.controller;
 
 import com.ssafy.enjoytrip.user.model.dto.User;
+import org.apache.tomcat.Jar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,12 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -36,40 +39,53 @@ public class UserController {
         return "user/login";
     }
 
+//    @PostMapping("login")
+//    public String login(User user, HttpSession session, RedirectAttributes ra,
+//                        HttpServletRequest request, HttpServletResponse response,
+//                        @RequestParam(name="saveId", required = false) String saveid) throws Exception {
+//        logger.debug("Post : login {}", user);
+//        User userInfo = service.getLogin(user);
+//        if(userInfo == null){
+//            //에러메시지도 출력시키자
+//            ra.addFlashAttribute("msg", "id와 비밀번호를 확인해주세요");
+//            return "redirect:login";
+//        }else {
+//            //유저 정보 세션에 등록
+//            session.setAttribute("userInfo", userInfo);
+//            //아이디 저장 정보 쿠키 추가
+//            if (saveid != null) {
+//                Cookie loginCookie = new Cookie("idCookie", user.getId());
+//                loginCookie.setMaxAge(60 * 60 * 24 * 7); // 쿠키 유효기간 7일
+//                // 쿠키를 HttpServletResponse 객체에 추가
+//                response.addCookie(loginCookie);
+//            } else {
+//                Cookie cookies[] = request.getCookies();
+//                if (cookies != null) {
+//                    for (Cookie cookie : cookies) {
+//                        if ("idCookie".equals(cookie.getName())) {
+//                            cookie.setMaxAge(0);
+//                            response.addCookie(cookie);
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//            return "redirect:/";
+//        }
+//    }
+
     @PostMapping("login")
-    public String login(User user, HttpSession session, RedirectAttributes ra,
-                        HttpServletRequest request, HttpServletResponse response,
-                        @RequestParam(name="saveId", required = false) String saveid) throws Exception {
-        logger.debug("Post : login {}", user);
-        User userInfo = service.getLogin(user);
-        if(userInfo == null){
-            //에러메시지도 출력시키자
-            ra.addFlashAttribute("msg", "id와 비밀번호를 확인해주세요");
-            return "redirect:login";
-        }else {
-            //유저 정보 세션에 등록
-            session.setAttribute("userInfo", userInfo);
-            //아이디 저장 정보 쿠키 추가
-            if (saveid != null) {
-                Cookie loginCookie = new Cookie("idCookie", user.getId());
-                loginCookie.setMaxAge(60 * 60 * 24 * 7); // 쿠키 유효기간 7일
-                // 쿠키를 HttpServletResponse 객체에 추가
-                response.addCookie(loginCookie);
-            } else {
-                Cookie cookies[] = request.getCookies();
-                if (cookies != null) {
-                    for (Cookie cookie : cookies) {
-                        if ("idCookie".equals(cookie.getName())) {
-                            cookie.setMaxAge(0);
-                            response.addCookie(cookie);
-                            break;
-                        }
-                    }
-                }
-            }
-            return "redirect:/";
-        }
+    @ResponseBody
+    public ResponseEntity<?> login(@RequestBody User user, @RequestBody String saveId) throws Exception {
+        logger.debug("Post : login {}, saveId {}", user, saveId);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("userInfo", service.getLogin(user));
+        result.put("saveId", saveId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+
     @GetMapping("logout")
     public String logout(HttpSession session){
         session.invalidate();
