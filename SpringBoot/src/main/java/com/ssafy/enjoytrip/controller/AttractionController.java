46951da65f32ce9580e379aca40615981b51d9f1
gmzuddl <>
2023-05-26 02:38:26 +0900

    [Update] : 정보 저장 아침에

--- SpringBoot/src/main/java/com/ssafy/enjoytrip/controller/AttractionController.java
+++ SpringBoot/src/main/java/com/ssafy/enjoytrip/controller/AttractionController.java
@@ -6,6 +6,7 @@ import com.ssafy.enjoytrip.model.attraction.service.AttractionInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,5 +41,11 @@ public class AttractionController {
         return service.attractionLists(paramMap);
     }
 
+
+    @PostMapping("savePlan/{userId}")
+    private ResponseEntity<?> savePlan(@PathVariable("userId") String userId,
+                                        @ResponseBody Map<String, Object> map) throws Exception{
+
+    }
 
 }
